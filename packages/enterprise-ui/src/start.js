2c2fa08e249fad5eb1551b700feb68b4c79254f4
FeHHix <ifenix0@gmail.com>
2020-06-27 21:05:30 +0300

    Fixed 301 redirect in a production mode.

--- packages/enterprise-ui/src/start.js
+++ packages/enterprise-ui/src/start.js
@@ -1,13 +1,44 @@
-const handler = require('serve-handler');
-const http = require('http');
+const compression = require('compression');
+const express = require('express');
+const path = require('path');
 const paths = require('../config/paths');
+const getWorkspaces = require('../config/getWorkspaces');
 
-const server = http.createServer((request, response) => {
-  // You pass two more arguments for config and middleware
-  // More details here: https://github.com/zeit/serve-handler#options
-  return handler(request, response, { public: paths.appBuild });
+const workspaces = getWorkspaces(paths.rootPackageJson, ['packages/*']);
+const routes = workspaces.map(({module}) => `/${module}`);
+
+const app = express();
+
+function shouldCompress(req, res) {
+  if (req.headers['x-no-compression']) return false;
+  return compression.filter(req, res);
+}
+
+app.use(
+  compression({
+      level: 2,
+      filter: shouldCompress,
+  })
+);
+
+app.get(['/', ...routes], function (req, res) {
+  res.status(200).sendFile(path.join(paths.appBuild, '/index.html'));
 });
 
-server.listen(3000, () => {
-  console.log('Running at http://localhost:3000');
+app.use(express.static(paths.appBuild, {index: false}));
+
+app.use((err, req, res, next) => {
+  if (req.xhr) {
+    res.status(500).send({
+      errorCode: 500,
+      message: 'Internal Server Error',
+    });
+  } else {
+    next(err);
+  }
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  const address = server.address();
+  console.log('Express server listening on port %d with pid %d', address.port, process.pid);
 });
